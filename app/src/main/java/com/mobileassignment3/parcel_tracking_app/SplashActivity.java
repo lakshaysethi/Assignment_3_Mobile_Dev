829f0f41136eb57fa4192ef8ee433845c923d997
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-06-27 18:43:35 +1200

    added do once to splash activity

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/SplashActivity.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/SplashActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.mobileassignment3.parcel_tracking_app.controllers.WriteToFireStore;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         // requestFeature() must be called before adding content
         // will hide the title
@@ -20,8 +24,6 @@ public class SplashActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         // show the activity in full screen
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        // change activity background color
-//        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
         setContentView(R.layout.activity_splash);
 
@@ -33,5 +35,23 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000); //means 3 seconds
+
+        doOnce();
+
     }
+
+    public void doOnce(){
+        try{
+
+
+            new WriteToFireStore().writeMasterDeliveryJobsToFirestore();
+
+
+
+
+        }catch (Exception e){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
