d88b49ed407f3e876a0bf3df2ae53eb1833f0a83
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-06-27 18:02:00 +1200

    removed google functions

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/FirebaseController.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/FirebaseController.java
@@ -54,46 +54,6 @@ public class FirebaseController {
     }
 
 
-    public void handleGoogleSignIn(GoogleSignInAccount account,Activity activity) {
-        try{
-
-            firebaseAuthWithGoogle(account.getIdToken());
-            FirebaseUser cu = mAuth.getCurrentUser();
-
-            if(cu!=null){
-                Toast.makeText(activity, "Welcome!"+ cu.getDisplayName(), Toast.LENGTH_SHORT).show();
-            }
-            updateUIafterLogin(activity,true);
-
-      
-
-        }catch (Exception e){
-            Toast.makeText(activity, account.getDisplayName(), Toast.LENGTH_SHORT).show();
-            Toast.makeText(activity, e.toString(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                           // Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                           // updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
     }
 
 
