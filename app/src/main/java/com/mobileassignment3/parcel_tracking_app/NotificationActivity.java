83fa9a95dda55c195defa3a23010fabc8da59173
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2020-06-27 18:11:45 +1200

    un do naming danica

--- app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
+++ app/src/main/java/com/mobileassignment3/parcel_tracking_app/NotificationActivity.java
@@ -29,7 +29,7 @@ public class NotificationActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.ic_person_pin_black_24dp);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
-        new FirebaseController().getUserDanica_s_function(new OnSuccessListener<User>() {
+        new FirebaseController().getUser(new OnSuccessListener<User>() {
             @Override
             public void onSuccess(User user) {
                 getSupportActionBar().setTitle(user.getUsername());
